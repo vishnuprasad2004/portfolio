b9aaac1b74498b00a7dbc0bac9ed5a6726103bee
vishnuprasad2004 <vishnuprasadkorada@gmail.com>
2024-10-07 01:24:21 +0530

    bug: key prop not added and extra lines

--- src/app/design/page.tsx
+++ src/app/design/page.tsx
@@ -1,15 +1,14 @@
 "use client";
 import SidePanel from "@/components/SidePanel";
-import { Space_Mono, Space_Grotesk, Jacquarda_Bastarda_9, Fraunces } from "next/font/google";
+import { Space_Grotesk, Fraunces } from "next/font/google";
 import { useEffect, useRef } from "react";
 import SplitType from "split-type";
 // import gsap from "gsap";
 import Image from "next/image";
 import designs from "@/lib/designs";
 
-const spaceMono = Space_Mono({weight:"400", subsets: ["latin"]})
+
 const spaceGrotesk = Space_Grotesk({weight:"700", subsets: ["latin"]})
-const jacquardaBastarda9 = Jacquarda_Bastarda_9({ weight: "400", subsets: ["latin"] })
 const fraunces = Fraunces({weight: "variable", subsets: ["latin"]})
 
 const DesignPage = () => {
@@ -17,7 +16,6 @@ const DesignPage = () => {
 
     useEffect(() => {
         const split = new SplitType(headingText.current!, { types: 'chars' });
-        const fonts = ['Arial', 'Courier New', 'Times New Roman', 'Georgia', 'Verdana', spaceMono.className, spaceGrotesk.className, jacquardaBastarda9.className];
     })
     return(
         <div className={"px-4"}>
@@ -29,10 +27,10 @@ const DesignPage = () => {
             {/* The display grid */}
 
             <div className="grid gap-6 lg:grid-cols-5 md:grid-cols-3 grid-cols-1 grid-auto-rows px-4 py-16 w-full text-center grid-flow-dense">
-                {designs.map((design) => {
-                    if(design.orientation === "landscape") return <Image src={design.src} alt={design.alt} height={0} width={200} objectFit="cover" loading="lazy" className="w-full lg:w-fit h-auto row-span-1 col-span-1 object-cover select-none hover:scale-105 duration-300 rounded-sm card"/>
-                    else if(design.orientation === "portrait") return <Image src={design.src} alt={design.alt} height={0} width={200} objectFit="cover" loading="lazy" className="w-full lg:w-fit h-auto row-span-[2] object-cover select-none hover:scale-105 duration-300 rounded-sm card"/>
-                    else return <Image src={design.src} alt={design.alt} height={0} width={200} objectFit="cover" loading="lazy" className="w-full lg:w-fit h-auto row-span-2 object-cover select-none hover:scale-105 duration-300 rounded-sm"/>
+                {designs.map((design,i) => {
+                    if(design.orientation === "landscape") return <Image key={i} src={design.src} alt={design.alt} height={0} width={200} objectFit="cover" loading="lazy" className="w-full lg:w-fit h-auto row-span-1 col-span-1 object-cover select-none hover:scale-105 duration-300 rounded-sm card"/>
+                    else if(design.orientation === "portrait") return <Image key={i} src={design.src} alt={design.alt} height={0} width={200} objectFit="cover" loading="lazy" className="w-full lg:w-fit h-auto row-span-[2] object-cover select-none hover:scale-105 duration-300 rounded-sm card"/>
+                    else return <Image key={i} src={design.src} alt={design.alt} height={0} width={200} objectFit="cover" loading="lazy" className="w-full lg:w-fit h-auto row-span-2 object-cover select-none hover:scale-105 duration-300 rounded-sm"/>
                 })}
             </div>
         </div>
