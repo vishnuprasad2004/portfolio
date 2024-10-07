0131b611751d9f08c278fec669585bb089a876f4
vishnuprasad2004 <vishnuprasadkorada@gmail.com>
2024-10-07 10:35:20 +0530

    feat: added designs and updates in sitemap

--- src/app/design/page.tsx
+++ src/app/design/page.tsx
@@ -21,16 +21,16 @@ const DesignPage = () => {
         <div className={"px-4"}>
             <SidePanel/>
             <div className="text-center pt-4">
-                <span className={fraunces.className + " text-8xl font-bold sticker select-none"} ref={headingText}>Gallery of Designs</span>
-                <p className={spaceGrotesk.className+" my-2"}>These are all the posters/banners I created for events or for club instagram using tools like Canva, Inkscape, and Photoshop.</p>
+                <span className={fraunces.className + " lg:text-8xl text-4xl font-bold sticker select-none"} ref={headingText}>Gallery of Designs</span>
+                <p className={spaceGrotesk.className+" my-5 text-xs lg:text-base"}>These are all the posters/banners I created for events or for club instagram using tools like Canva, Inkscape, and Photoshop.</p>
             </div>
             {/* The display grid */}
 
             <div className="grid gap-6 lg:grid-cols-5 md:grid-cols-3 grid-cols-1 grid-auto-rows px-4 py-16 w-full text-center grid-flow-dense">
                 {designs.map((design,i) => {
-                    if(design.orientation === "landscape") return <Image key={i} src={design.src} alt={design.alt} height={0} width={200} objectFit="cover" loading="lazy" className="w-full lg:w-fit h-auto row-span-1 col-span-1 object-cover select-none hover:scale-105 duration-300 rounded-sm card"/>
-                    else if(design.orientation === "portrait") return <Image key={i} src={design.src} alt={design.alt} height={0} width={200} objectFit="cover" loading="lazy" className="w-full lg:w-fit h-auto row-span-[2] object-cover select-none hover:scale-105 duration-300 rounded-sm card"/>
-                    else return <Image key={i} src={design.src} alt={design.alt} height={0} width={200} objectFit="cover" loading="lazy" className="w-full lg:w-fit h-auto row-span-2 object-cover select-none hover:scale-105 duration-300 rounded-sm"/>
+                    if(design.orientation === "landscape") return <Image key={i} src={design.src} alt={design.alt} title={design.alt} height={0} width={200} objectFit="cover" loading="lazy" className="w-full lg:w-fit h-auto row-span-1 col-span-1 object-cover select-none lg:hover:scale-105 duration-300 rounded-sm card"/>
+                    else if(design.orientation === "portrait") return <Image key={i} src={design.src} alt={design.alt} title={design.alt} height={0} width={200} objectFit="cover" loading="lazy" className="w-full lg:w-fit h-auto row-span-[2] object-cover select-none lg:hover:scale-105 duration-300 rounded-sm card"/>
+                    else return <Image key={i} src={design.src} alt={design.alt} title={design.alt} height={0} width={200} objectFit="cover" loading="lazy" className="w-full lg:w-fit h-auto row-span-2 object-cover select-none lg:hover:scale-105 duration-300 rounded-sm"/>
                 })}
             </div>
         </div>
--- src/app/sitemap.ts
+++ src/app/sitemap.ts
@@ -3,10 +3,16 @@ import type { MetadataRoute } from 'next'
 export default function sitemap(): MetadataRoute.Sitemap {
     return [
         {
-        url: 'https://vishnu-prasad-korada.vercel.app',
-        lastModified: new Date(),
-        changeFrequency: 'monthly',
-        priority: 1,
+            url: 'https://vishnu-prasad-korada.vercel.app',
+            lastModified: new Date(),
+            changeFrequency: 'monthly',
+            priority: 1,
+        }, 
+        {
+            url: 'https://vishnu-prasad-korada.vercel.app/design',
+            lastModified: new Date(),
+            changeFrequency: 'monthly',
+            priority: 0.8,
         }
     ]
 }
