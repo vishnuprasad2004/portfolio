a03a0bd9a09e3d40f6ef2e34ec57216d857d32dd
vishnuprasad2004 <vishnuprasadkorada@gmail.com>
2024-10-07 16:36:02 +0530

    fixed the downloadable poster problem

--- src/app/design/page.tsx
+++ src/app/design/page.tsx
@@ -1,15 +1,29 @@
 "use client";
 import SidePanel from "@/components/SidePanel";
 import { Space_Grotesk, Fraunces } from "next/font/google";
-import { useEffect, useRef } from "react";
+import { FC, useEffect, useRef } from "react";
 import SplitType from "split-type";
 // import gsap from "gsap";
 import Image from "next/image";
 import designs from "@/lib/designs";
 
 
+interface DesignPosterProp {
+    index: number
+    src: string,
+    orientation: "portrait" | "landscape" | "square",
+    alt: string
+}
+const DesignPoster:FC<DesignPosterProp> = ({index, src, orientation, alt}: DesignPosterProp) => {
+
+    if(orientation === "landscape") return <Image key={index} src={src} alt={alt} title={alt} draggable={false} onContextMenu={() => { return false }} height={0} width={200} loading="lazy" className="w-full lg:w-fit h-auto row-span-1 col-span-1 object-cover select-none lg:hover:scale-105 duration-300 rounded-sm card"/>
+    else if(orientation === "portrait") return <Image key={index} src={src} alt={alt} title={alt} draggable={false} onContextMenu={() => { return false }} height={0} width={200} loading="lazy" className="w-full lg:w-fit h-auto row-span-2 object-cover select-none lg:hover:scale-105 duration-300 rounded-sm card"/>
+    else return <Image key={index} src={src} alt={alt} title={alt} draggable={false} onContextMenu={() => { return false }} height={0} width={200} loading="lazy" className="w-full lg:w-fit h-auto row-span-2 object-cover select-none lg:hover:scale-105 duration-300 rounded-sm"/>
+
+}
+
 const spaceGrotesk = Space_Grotesk({weight:"700", subsets: ["latin"]})
-const fraunces = Fraunces({weight: "variable", subsets: ["latin"]})
+const fraunces = Fraunces({weight: "variable", subsets: ["latin"], style:"italic"})
 
 const DesignPage = () => {
     const headingText = useRef(null);
@@ -21,16 +35,14 @@ const DesignPage = () => {
         <div className={"px-4"}>
             <SidePanel/>
             <div className="text-center pt-4">
-                <span className={fraunces.className + " lg:text-8xl text-4xl font-bold sticker select-none"} ref={headingText}>Gallery of Designs</span>
+                <span className={fraunces.className + " lg:text-8xl text-4xl font-bold italic sticker select-none"} ref={headingText}>Gallery of Designs</span>
                 <p className={spaceGrotesk.className+" my-5 text-xs lg:text-base"}>These are all the posters/banners I created for events or for club instagram using tools like Canva, Inkscape, and Photoshop.</p>
             </div>
             {/* The display grid */}
 
-            <div className="grid gap-6 lg:grid-cols-5 md:grid-cols-3 grid-cols-1 grid-auto-rows px-4 py-16 w-full text-center grid-flow-dense">
+            <div className="grid lg:gap-6 gap-2 lg:grid-cols-5 md:grid-cols-3 grid-cols-2 grid-auto-rows lg:px-4 py-16 w-full text-center grid-flow-dense">
                 {designs.map((design,i) => {
-                    if(design.orientation === "landscape") return <Image key={i} src={design.src} alt={design.alt} title={design.alt} height={0} width={200} objectFit="cover" loading="lazy" className="w-full lg:w-fit h-auto row-span-1 col-span-1 object-cover select-none lg:hover:scale-105 duration-300 rounded-sm card"/>
-                    else if(design.orientation === "portrait") return <Image key={i} src={design.src} alt={design.alt} title={design.alt} height={0} width={200} objectFit="cover" loading="lazy" className="w-full lg:w-fit h-auto row-span-[2] object-cover select-none lg:hover:scale-105 duration-300 rounded-sm card"/>
-                    else return <Image key={i} src={design.src} alt={design.alt} title={design.alt} height={0} width={200} objectFit="cover" loading="lazy" className="w-full lg:w-fit h-auto row-span-2 object-cover select-none lg:hover:scale-105 duration-300 rounded-sm"/>
+                    return <DesignPoster key={i} index={i} src={design.src} orientation={design.orientation} alt={design.alt}/>
                 })}
             </div>
         </div>
